c054fa8b907f4766c8300574a4b175907a05c59f
Vakulenko Aleksei <vakulenko_alex@mail.ru>
2025-04-28 18:20:46 +0400

    оформление

--- app/src/main/java/com/vakulenkoalex/moneygateway/MainActivity.kt
+++ app/src/main/java/com/vakulenkoalex/moneygateway/MainActivity.kt
@@ -2,7 +2,6 @@ package com.vakulenkoalex.moneygateway
 
 import android.Manifest
 import android.app.Application
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
