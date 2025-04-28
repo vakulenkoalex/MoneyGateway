ab217f03470ffa91d2fcdd1b5dd7c775a46a21cb
Vakulenko Aleksei <vakulenko_alex@mail.ru>
2025-04-28 17:51:32 +0400

    удалил неиспользуемые импорты

--- app/src/main/java/com/vakulenkoalex/moneygateway/SmsViewModel.kt
+++ app/src/main/java/com/vakulenkoalex/moneygateway/SmsViewModel.kt
@@ -1,9 +1,6 @@
 package com.vakulenkoalex.moneygateway
 
 import android.app.Application
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 
