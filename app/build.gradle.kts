92113ab9fa99a0dc8ffa37045980259d007b149f
Vakulenko Aleksei <vakulenko_alex@mail.ru>
2025-04-30 20:41:42 +0400

    поднял версию

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.vakulenkoalex.moneygateway"
         minSdk = 34
         targetSdk = 35
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
     }
 
     buildTypes {
